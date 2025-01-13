dc15275fa81fd67e5a8cfd93abb05f2304574bbb
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-13 14:40:11 +0100

    Add → Basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./assets/layout/DefaultLayout";
+import HomePage from "./assets/pages/HomePage";
+import AboutUsPage from "./assets/pages/AboutUsPage";
+import MoviesPage from "./assets/pages/MoviesPage";
+import RandomPage from "./assets/pages/RandomPage";
+
 export default function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/">
+            <Route index Component={HomePage} />
+            <Route path="/aboutus" Component={AboutUsPage} />
+            <Route path="/movies" Component={MoviesPage} />
+            <Route path="/random" Component={RandomPage} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
