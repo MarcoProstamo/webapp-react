04adc011f4ef02f4f4589280ed661d803d380709
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-14 12:21:47 +0100

    Add → Navbar Style

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,10 +2,11 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          Navbar
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <Link to="/" className="navbar-brand d-flex">
+          <img width="75px" src="/logo.png" alt="logo" />
+          <span className="align-self-center fs-2 fw-semibold">The Movie</span>
         </Link>
         <button
           className="navbar-toggler"
@@ -18,8 +19,11 @@ export default function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div
+          className="collapse navbar-collapse flex-grow-0"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-color">
             <li>
               <NavLink className="navlink" to="/">
                 Home
@@ -41,7 +45,7 @@ export default function Navbar() {
               </NavLink>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
@@ -51,7 +55,7 @@ export default function Navbar() {
             <button className="btn btn-outline-success" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </div>
     </nav>
